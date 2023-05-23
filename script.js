9a991587e25bfd48939688458e8af92d6aabdedb
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-05-23 13:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 function palindrome(str){
  let j = str.length - 1;
+	if(str.length/2==0)
+	{
     for (let i = 0; i < j / 2; i++) {
         let x = str[i];
         let y = str[j - i];
@@ -12,5 +14,18 @@ function palindrome(str){
     }
  
     return true;
+	}else
+	{
+		 for (let i = 0; (i < j / 2) + 1; i++) {
+        let x = str[i];
+        let y = str[j - i];
+        if (x != y) {
+           
+            return false;
+        }
+    }
+ 
+    return true;
+	}
 }
 module.exports = palindrome
