21b04720e136fdfd555d806298cdb39d8739ee24
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-05-23 14:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 // complete the given function
 
 function palindrome(str){
- let j = str.length - 1;
-	if(str.length/2==0)
-	{
-    for (let i = 0; i < j / 2; i++) {
-        let x = str[i];
-        let y = str[j - i];
-        if (x != y) {
-           
+ onst arrayValues = str.split('');
+
+    
+    const reverseArrayValues = arrayValues.reverse();
+
+    
+    const reverseString = reverseArrayValues.join('');
+           if(str != reverseString) {
             return false;
+		   }
         }
     }
  
     return true;
-	}else
-	{
-		 for (let i = 0; (i < j / 2) + 1; i++) {
-        let x = str[i];
-        let y = str[j - i];
-        if (x != y) {
-           
-            return false;
-        }
-    }
+	
  
-    return true;
-	}
+    
 }
 module.exports = palindrome
