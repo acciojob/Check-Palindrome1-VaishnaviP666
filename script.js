269cd7e431809cc70629a7ee78a5cbf8cf58b718
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-05-23 13:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,16 @@
 function palindrome(str){
  let j = str.length - 1;
     for (let i = 0; i < j / 2; i++) {
-        let x = str[i];//forward character
-        let y = str[j - i];//backward character
+        let x = str[i];
+        let y = str[j - i];
         if (x != y) {
-            // return false if string not match
+           
             return false;
+		else
+			 return true;
         }
     }
-    /// return true if string is palindrome
-    return true;
+ 
+   
 }
 module.exports = palindrome
